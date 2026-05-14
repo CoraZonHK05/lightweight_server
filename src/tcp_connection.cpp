47be48b1dc5b923233c888c697a4472b829ef937
CorazonHK <aurora05cora@gmail.com>
2026-05-14 23:04:23 +0800

    Update tcp_connection.cpp

--- src/tcp_connection.cpp
+++ src/tcp_connection.cpp
@@ -1,11 +1,15 @@
 #include "tcp_connection.h"
 
 #ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
 #include <winsock2.h>
+#include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 #define close closesocket
-#undef send
-#undef recv
+#else
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #endif
 
 #include <iostream>
